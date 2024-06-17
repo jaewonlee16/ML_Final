47d098b627fb7fa8978b69862aa7b606a1e95a8c
jaewonlee16 <19snut@gmail.com>
2024-06-17 19:44:30 +0900

    pad sequence 10

--- data_utils.py
+++ data_utils.py
@@ -17,12 +17,18 @@ class MLDataset(Dataset):
         imgs = np.load(f"{self.path}/{idx}.npy")
         seq_length = imgs.shape[0]
         label = self.labels[str(idx)]
-        
+        # Padding images to length 10
+        if seq_length < 10:
+            padding = np.zeros((10 - seq_length, 28, 28, 3))
+            imgs = np.vstack((imgs, padding))
+        if len(label) < 10:
+            label.extend([0] * (10 - len(label)))  # Assuming 0 is the padding index for labels
         return torch.tensor(imgs, dtype=torch.float32), torch.tensor(label, dtype=torch.long), seq_length
 
 
 def collate_fn(batch):
     sequences, targets, lengths = zip(*batch)
+    #print(sequences[0].shape)
     
     padded_sequences = pad_sequence([seq for seq in sequences], batch_first=True)
     padded_targets = pad_sequence([tar for tar in targets], batch_first=True)
