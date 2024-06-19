b6cbc1b2415462a51e4ec434d3b51fd3d8bc5457
jaewonlee16 <19snut@gmail.com>
2024-06-19 23:20:57 +0900

    beam search

--- modeling.py
+++ modeling.py
@@ -550,81 +550,324 @@ class TransformerSeq2Seq(nn.Module):
         ##############################################################################
         return logits
 
-    def generate(self, inputs, lengths, inp_seq, max_length):
-        """
-        inputs: (Batch_size, Sequence_length, Height, Width, Channel)
-        lengths: (Batch_size,)
-        inp_seq: (Batch_size, 1)
-        max_length -> a single integer number (ex. 10)
-        generated_tok: (Batch_size, max_length) -> long dtype tensor
-        
-        ##############################################################################
-        #                          IMPLEMENT YOUR CODE                               #
-        ##############################################################################
-        # Problem 4: design generate function of Seq2SeqModel
+    
+    
+    def generate(self, inputs, lengths, inp_seq, max_length, num_beams=5, pad_token_id=0):
 
-        encoder_outputs = self.encoder(inputs, lengths)
+        encoder_output = self.encoder(inputs, lengths).permute(1, 0, 2)
+
+        batch_size, seq_len, d_embed = encoder_output.size()
+        encoder_output = encoder_output.unsqueeze(1).repeat(1, num_beams, 1, 1).view(batch_size * num_beams, seq_len, d_embed)
+
+        beam_scorer = BeamSearchScorer(
+            batch_size=batch_size,
+            max_length=max_length,
+            num_beams=num_beams,
+            device=inputs.device,
+            length_penalty=1.0,
+            do_early_stopping=False,
+        )
+
+
+
+        input_ids = torch.ones((batch_size * num_beams, 1), device=inputs.device).long()
+        input_ids = input_ids * inp_seq[:, :1].repeat(num_beams, 1)
         
-        # Initialize the input for the decoder with the start token
-        dec_input = inp_seq
         
-        generated_tok = []
-        
-        for t in range(max_length):
-            logits = self.decoder(dec_input, encoder_outputs)
-            predicted_token = logits.argmax(2)
-            generated_tok.append(predicted_token)
-            
-            dec_input = predicted_token
-        
-        generated_tok = torch.cat(generated_tok, dim=1)
-        """
-        encoder_output = self.encoder(inputs, lengths)
-        generated_tok = inp_seq
+
+        beam_scores = torch.zeros((batch_size, num_beams), device=inputs.device)
+        beam_scores[:, 1:] = -1e9
+
+        beam_scores = beam_scores.view(-1)
 
         for step in range(max_length):
-            tgt_mask = self.decoder.generate_square_subsequent_mask(generated_tok.size(1)).to(inp_seq.device)
-            tgt_key_padding_mask = self.decoder.create_pad_mask(generated_tok, 0).to(inp_seq.device)
-            output = self.decoder(generated_tok, encoder_output, tgt_mask=tgt_mask, tgt_key_padding_mask=tgt_key_padding_mask)
-            predicted_token = output[:, -1:].argmax(2)
-            generated_tok = torch.cat((generated_tok, predicted_token), dim=1)
 
-        generated_tok = self.remove_zeros_shift_left(generated_tok)
-        return generated_tok[:, 1:]
-        
-        ##############################################################################
-        #                          END OF YOUR CODE                                  #
-        ##############################################################################
-        return generated_tok
+            tgt_mask = self.decoder.generate_square_subsequent_mask(input_ids.size(1)).to(inputs.device)
+            tgt_key_padding_mask = self.decoder.create_pad_mask(input_ids, pad_token_id).to(inputs.device)
+
+
+            outputs = self.decoder(input_ids, encoder_output.permute(1, 0, 2), tgt_mask=None, tgt_key_padding_mask=tgt_key_padding_mask)
+
+            next_token_logits = outputs[:, -1, :]
+
+            next_token_scores = F.log_softmax(next_token_logits, dim=-1)
+
+
+            next_token_scores = next_token_scores + beam_scores[:, None].expand_as(next_token_scores)
+            vocab_size = next_token_scores.size(-1)
+
+            next_token_scores = next_token_scores.view(batch_size, num_beams * vocab_size)
+
+
+            next_scores, next_tokens = torch.topk(next_token_scores, 2 * num_beams, dim=1, largest=True, sorted=True)
+            next_indices = next_tokens // vocab_size
+            next_tokens = next_tokens % vocab_size
+
+            beam_outputs = beam_scorer.process(
+                input_ids, next_scores, next_tokens, next_indices,
+                pad_token_id=pad_token_id
+            )
+
+            input_ids = torch.cat([input_ids[beam_outputs["next_beam_indices"], :], beam_outputs["next_beam_tokens"].unsqueeze(-1)], dim=-1)
+            beam_scores = beam_outputs["next_beam_scores"]
+
+            if beam_scorer.is_done:
+                break
+
+        final_outputs = beam_scorer.finalize(input_ids, beam_scores, next_tokens, next_indices, pad_token_id=pad_token_id)
+
+        return final_outputs[:, 1:]
     
-    def pad_and_create_mask(self, inputs, lengths):
-        batch_size, seq_len, h, w, c = inputs.shape
-        max_seq_len = max(lengths)
-        
-        padded_inputs = torch.zeros((batch_size, max_seq_len, h, w, c), device=inputs.device)
-        src_key_padding_mask = torch.ones((batch_size, max_seq_len), device=inputs.device, dtype=torch.bool)
-        
-        for i, length in enumerate(lengths):
-            padded_inputs[i, :length] = inputs[i, :length]
-            src_key_padding_mask[i, :length] = False
-        
-        return padded_inputs, src_key_padding_mask
-    
-    def remove_zeros_shift_left(self, tensor):
-        # List to store the new rows
-        new_rows = []
-    
-        for row in tensor:
-            # Filter out the zeros
-            non_zeros = row[row != 0]
-            # Calculate the number of zeros to append
-            num_zeros = len(row) - len(non_zeros)
-            # Create the new row with non-zeros followed by zeros
-            new_row = torch.cat((non_zeros, torch.zeros(num_zeros, dtype=row.dtype, device=row.device)))
-            new_rows.append(new_row)
-    
-        # Stack the new rows to form the modified tensor
-        new_tensor = torch.stack(new_rows).to(self.device)
-    
-        return new_tensor
+from abc import *
+from collections import UserDict
+
+
+
+# This code is from huggingface
+@abstractmethod
+class BeamScorer(ABC):
+    """
+    Abstract base class for all beam scorers that are used for :meth:`~transformers.PretrainedModel.beam_search` and
+    :meth:`~transformers.PretrainedModel.beam_sample`.
+    """
+
+    def process(
+        self,
+        input_ids: torch.LongTensor,
+        next_scores: torch.FloatTensor,
+        next_tokens: torch.LongTensor,
+        next_indices: torch.LongTensor,
+        **kwargs
+    ):
+        raise NotImplementedError("This is an abstract method.")
+
+    def finalize(
+        self,
+        input_ids: torch.LongTensor,
+        next_scores: torch.FloatTensor,
+        next_tokens: torch.LongTensor,
+        next_indices: torch.LongTensor,
+        **kwargs
+    ) -> torch.LongTensor:
+        raise NotImplementedError("This is an abstract method.")
+
+
+
+class BeamSearchScorer(BeamScorer):
+    def __init__(
+        self,
+        batch_size: int,
+        max_length: int,
+        num_beams: int,
+        device: torch.device,
+        length_penalty = 1.0,
+        do_early_stopping  = False,
+        num_beam_hyps_to_keep = 1,
+    ):
+        self.max_length = max_length + 1
+        self.num_beams = num_beams
+        self.device = device
+        self.length_penalty = length_penalty
+        self.do_early_stopping = do_early_stopping
+        self.num_beam_hyps_to_keep = num_beam_hyps_to_keep
+
+        self._is_init = False
+        self._beam_hyps = [
+            BeamHypotheses(
+                num_beams=self.num_beams,
+                max_length=self.max_length,
+                length_penalty=self.length_penalty,
+                early_stopping=self.do_early_stopping,
+            )
+            for _ in range(batch_size)
+        ]
+        self._done = torch.tensor([False for _ in range(batch_size)], dtype=torch.bool, device=self.device)
+
+        if not isinstance(num_beams, int) or num_beams <= 1:
+            raise ValueError(
+                f"`num_beams` has to be an integer strictly greater than 1, but is {num_beams}. For `num_beams` == 1, one should make use of `greedy_search` instead."
+            )
+
+    @property
+    def is_done(self) -> bool:
+        return self._done.all()
+
+    def process(
+        self,
+        input_ids: torch.LongTensor,
+        next_scores: torch.FloatTensor,
+        next_tokens: torch.LongTensor,
+        next_indices: torch.LongTensor,
+        pad_token_id = None,
+        eos_token_id = None,
+    ):
+        cur_len = input_ids.shape[-1]
+        batch_size = len(self._beam_hyps)
+        assert batch_size == (input_ids.shape[0] // self.num_beams)
+
+        device = input_ids.device
+        next_beam_scores = torch.zeros((batch_size, self.num_beams), dtype=next_scores.dtype, device=device)
+        next_beam_tokens = torch.zeros((batch_size, self.num_beams), dtype=next_tokens.dtype, device=device)
+        next_beam_indices = torch.zeros((batch_size, self.num_beams), dtype=next_indices.dtype, device=device)
+
+        for batch_idx, beam_hyp in enumerate(self._beam_hyps):
+            if self._done[batch_idx]:
+                assert (
+                    len(beam_hyp) >= self.num_beams
+                ), "Batch can only be done if at least {} beams have been generated".format(self.num_beams)
+                assert (
+                    eos_token_id is not None and pad_token_id is not None
+                ), "generated beams >= num_beams -> eos_token_id and pad_token have to be defined"
+                # pad the batch
+                next_beam_scores[batch_idx, :] = 0
+                next_beam_tokens[batch_idx, :] = pad_token_id
+                next_beam_indices[batch_idx, :] = 0
+                continue
+
+            # next tokens for this sentence
+            beam_idx = 0
+            for beam_token_rank, (next_token, next_score, next_index) in enumerate(
+                zip(next_tokens[batch_idx], next_scores[batch_idx], next_indices[batch_idx])
+            ):
+                batch_beam_idx = batch_idx * self.num_beams + next_index
+                # add to generated hypotheses if end of sentence
+                if (eos_token_id is not None) and (next_token.item() == eos_token_id):
+                    # if beam_token does not belong to top num_beams tokens, it should not be added
+                    is_beam_token_worse_than_top_num_beams = beam_token_rank >= self.num_beams
+                    if is_beam_token_worse_than_top_num_beams:
+                        continue
+                    beam_hyp.add(
+                        input_ids[batch_beam_idx].clone(),
+                        next_score.item(),
+                    )
+                else:
+                    # add next predicted token since it is not eos_token
+                    next_beam_scores[batch_idx, beam_idx] = next_score
+                    next_beam_tokens[batch_idx, beam_idx] = next_token
+                    next_beam_indices[batch_idx, beam_idx] = batch_beam_idx
+                    beam_idx += 1
+
+                # once the beam for next step is full, don't add more tokens to it.
+                if beam_idx == self.num_beams:
+                    break
+
+            if beam_idx < self.num_beams:
+                raise ValueError(
+                    f"At most {self.num_beams} tokens in {next_tokens[batch_idx]} can be equal to `eos_token_id: {eos_token_id}`. Make sure {next_tokens[batch_idx]} are corrected."
+                )
+
+            # Check if we are done so that we can save a pad step if all(done)
+            self._done[batch_idx] = self._done[batch_idx] or beam_hyp.is_done(
+                next_scores[batch_idx].max().item(), cur_len
+            )
+
+        return UserDict(
+            {
+                "next_beam_scores": next_beam_scores.view(-1),
+                "next_beam_tokens": next_beam_tokens.view(-1),
+                "next_beam_indices": next_beam_indices.view(-1),
+            }
+        )
+
+
+    def finalize(
+        self,
+        input_ids: torch.LongTensor,
+        final_beam_scores: torch.FloatTensor,
+        final_beam_tokens: torch.LongTensor,
+        final_beam_indices: torch.LongTensor,
+        pad_token_id = None,
+        eos_token_id = None,
+    ) -> torch.LongTensor:
+        batch_size = len(self._beam_hyps)
+
+        # finalize all open beam hypotheses and add to generated hypotheses
+        for batch_idx, beam_hyp in enumerate(self._beam_hyps):
+            if self._done[batch_idx]:
+                continue
+
+            # need to add best num_beams hypotheses to generated hyps
+            for beam_id in range(self.num_beams):
+                batch_beam_idx = batch_idx * self.num_beams + beam_id
+                final_score = final_beam_scores[batch_beam_idx].item()
+                final_tokens = input_ids[batch_beam_idx]
+                beam_hyp.add(final_tokens, final_score)
+
+        # select the best hypotheses
+        sent_lengths = input_ids.new(batch_size * self.num_beam_hyps_to_keep)
+        best = []
+
+        # retrieve best hypotheses
+        for i, beam_hyp in enumerate(self._beam_hyps):
+            sorted_hyps = sorted(beam_hyp.beams, key=lambda x: x[0])
+            for j in range(self.num_beam_hyps_to_keep):
+                best_hyp = sorted_hyps.pop()[1]
+                sent_lengths[self.num_beam_hyps_to_keep * i + j] = len(best_hyp)
+                best.append(best_hyp)
+
+        # prepare for adding eos
+        sent_max_len = min(sent_lengths.max().item() + 1, self.max_length)
+        decoded: torch.LongTensor = input_ids.new(batch_size * self.num_beam_hyps_to_keep, sent_max_len)
+        # shorter batches are padded if needed
+        if sent_lengths.min().item() != sent_lengths.max().item():
+            assert pad_token_id is not None, "`pad_token_id` has to be defined"
+            decoded.fill_(pad_token_id)
+
+        # fill with hypotheses and eos_token_id if the latter fits in
+        for i, hypo in enumerate(best):
+            decoded[i, : sent_lengths[i]] = hypo
+            if sent_lengths[i] < self.max_length:
+                decoded[i, sent_lengths[i]] = eos_token_id
+        return decoded
+
+
+
+class BeamHypotheses:
+    def __init__(self, num_beams: int, max_length: int, length_penalty: float, early_stopping: bool):
+        """
+        Initialize n-best list of hypotheses.
+        """
+        self.max_length = max_length - 1  # ignoring bos_token
+        self.length_penalty = length_penalty
+        self.early_stopping = early_stopping
+        self.num_beams = num_beams
+        self.beams = []
+        self.worst_score = 1e9
+
+    def __len__(self):
+        """
+        Number of hypotheses in the list.
+        """
+        return len(self.beams)
+
+    def add(self, hyp: torch.LongTensor, sum_logprobs: float):
+        """
+        Add a new hypothesis to the list.
+        """
+        score = sum_logprobs / (hyp.shape[-1] ** self.length_penalty)
+        if len(self) < self.num_beams or score > self.worst_score:
+            self.beams.append((score, hyp))
+            if len(self) > self.num_beams:
+                sorted_next_scores = sorted([(s, idx) for idx, (s, _) in enumerate(self.beams)])
+                del self.beams[sorted_next_scores[0][1]]
+                self.worst_score = sorted_next_scores[1][0]
+            else:
+                self.worst_score = min(score, self.worst_score)
+
+    def is_done(self, best_sum_logprobs: float, cur_len: int) -> bool:
+        """
+        If there are enough hypotheses and that none of the hypotheses being generated can become better than the worst
+        one in the heap, then we are done with this sentence.
+        """
+
+        if len(self) < self.num_beams:
+            return False
+        elif self.early_stopping:
+            return True
+        else:
+            cur_score = best_sum_logprobs / cur_len ** self.length_penalty
+            ret = self.worst_score >= cur_score
+            return ret
+
+
     
