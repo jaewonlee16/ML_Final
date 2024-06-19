23c02b05eba6464d287d79156232c53a311f620e
jaewonlee16 <19snut@gmail.com>
2024-06-19 15:31:12 +0900

    deleted torchvision transforms

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,7 @@
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
-from torchvision import transforms
+#from torchvision import transforms
 
 import random
 
@@ -27,9 +27,11 @@ class trainMLDataset(Dataset):
         imgs = torch.tensor(imgs, dtype=torch.float32)
         
         # Apply data augmentation to each image if transform is provided
+        """
         if self.transform:
 
             imgs = np.array([self.transform(transforms.ToPILImage()(img.permute(2, 0, 1))).permute(1, 2, 0) for img in imgs])
+        """
             
 
         # Padding images and labels to length 10 at random positions
@@ -76,22 +78,25 @@ class MLDataset(Dataset):
 
 def collate_fn(batch, transform=None):
     sequences, targets, lengths = zip(*batch)
+    """
     if transform:
         augmented_sequences = []
         for seq in sequences:
             augmented_seq = np.array([np.array(transform(transforms.ToPILImage()(img.permute(2, 0, 1))).permute(1, 2, 0)) for img in seq])
             augmented_sequences.append(torch.tensor(augmented_seq, dtype=torch.float32))
         sequences = augmented_sequences
+    """
     #print(sequences.shape)
     padded_sequences = pad_sequence([seq for seq in sequences], batch_first=True)
     padded_targets = pad_sequence([tar for tar in targets], batch_first=True)
     
     return padded_sequences, padded_targets, torch.tensor(lengths, dtype=torch.float32)
 
-
+"""
 augmentation_transforms = transforms.Compose([
     transforms.RandomRotation(20),  # Rotate the image by up to 10 degrees
     transforms.RandomResizedCrop(28, scale=(1.0, 1.2)),  # Randomly crop and resize the image
     transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),  # Random color adjustments
     transforms.ToTensor()  # Convert the image to a tensor
 ])
+"""
